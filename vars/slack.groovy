0d108a0e615732a45cff3c4ca80d272f6ad7ed68
abithasrini <96717342+abithasrini@users.noreply.github.com>
2022-12-13 17:14:46 +0530

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -4,7 +4,7 @@
 //   if ( buildResult == "SUCCESS" ) {
 //     #slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
     
-  }
+//   }
 //   else if( buildResult == "FAILURE" ) { 
 //     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
 //   }
